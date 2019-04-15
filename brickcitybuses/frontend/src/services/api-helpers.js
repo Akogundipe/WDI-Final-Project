de9b379467ef3b5edf7a370ab070bd7ac62fdf18
Akinyemi Ogundipe <ijnuthomefolder@ool-ad02062f.dyn.optonline.net>
2019-04-15 11:13:01 -0400

    added it to another base url

--- brickcitybuses/frontend/src/services/api-helpers.js
+++ brickcitybuses/frontend/src/services/api-helpers.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/'
+  //baseURL: 'http://localhost:3000/'
+  baseURL: 'https://infinite-coast-14557.herokuapp.com/'
 });
 
 const updateToken = (token) => {
